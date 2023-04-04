76c9025f6691596d229081dd98fba2c663bcd57d
211ShyN <211shyn@gmail.com>
2023-04-04 12:25:30 +0900

    second commit

--- grade calc.py
+++ grade calc.py
@@ -1,15 +1,40 @@
-#학점계산기
+#평점변환함수
+def convert_score(grade):
+    match grade:
+        case 'A+':
+            gpa = 4.5
+        case 'A0':
+            gpa = 4.0
+        case 'B+':
+            gpa = 3.5
+        case 'B0':
+            gpa = 3.0
+        case 'C+':
+            gpa = 2.5
+        case 'C0':
+            gpa = 2.0
+        case 'D+:
+            gpa = 1.5
+        case 'D':
+            gpa = 1.0
+        case 'F':
+            gpa = 0.0
+            
 
-총학점 = 0
-
-while True: 
+while True:
     print("작업을 선택하세요.")
+    print("     1. 입력")
+    print("     2. 계산")
 
-    작업 = input("1.입력\n2.계산\n")
+    user_value = input()
+    
+    match value:
+        #연산
+        case 1:
+            user_value = input("학점을 입력하세요: ")
+            credit = int(user_value)
 
-    if 작업 == '1':
-        학점 = input("학점을 입력하세요:")
-        평점 = input("평점을 입력하세요:")
-        print("입력되었습니다.")
-        
+            user_value = input("평점을 입력하세요: ")
+            gpa = convert_score(user_value)
 
+            if gpa = 0
