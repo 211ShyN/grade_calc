7d1c1ac9db0dde979be5311dabbce8cc964e152a
211ShyN <211shyn@gmail.com>
2023-04-04 15:31:20 +0900

    third commit

--- grade calc.py
+++ grade calc.py
@@ -13,13 +13,18 @@ def convert_score(grade):
             gpa = 2.5
         case 'C0':
             gpa = 2.0
-        case 'D+:
+        case 'D+':
             gpa = 1.5
         case 'D':
             gpa = 1.0
         case 'F':
             gpa = 0.0
-            
+    return gpa
+
+#입력
+
+archive_credit, submit_credit = 0, 0
+archive_gpa, submit_gpa = 0.0, 0.0
 
 while True:
     print("작업을 선택하세요.")
@@ -27,6 +32,7 @@ while True:
     print("     2. 계산")
 
     user_value = input()
+    value = int(user_value)
     
     match value:
         #연산
@@ -37,4 +43,16 @@ while True:
             user_value = input("평점을 입력하세요: ")
             gpa = convert_score(user_value)
 
-            if gpa = 0
+            if gpa > 0:
+                submit_credit += credit
+                submit_gpa =+ credit * gpa
+            archive_credit += credit
+            archive_gpa =+ credit * gpa
+
+        #출력
+        case 2:
+            submit_gpa /= submit_credit
+            archive_gpa /= archive_credit
+            print('제출용: ' + str(submit_credit) + '학점 (GPA: ' + str(submit_gpa) + ')')
+            print('열람용: ' + str(archive_credit) + '학점 (GPA: ' + str(archive_gpa) + ')')
+            break
